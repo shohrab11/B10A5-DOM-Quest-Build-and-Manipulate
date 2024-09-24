814fa8f20b0991e5ba2c2fc4d4657d8e8801d67e
shohrab11 <shohrabaiub7@gmail.com>
2024-09-24 21:57:06 +0600

    history toggle done

--- feature.js
+++ feature.js
@@ -1,11 +1,15 @@
 document.getElementById('donation-btn')
     .addEventListener('click', function () {
+        document.getElementById('donation-btn').classList.add('bg-green-300');
+        document.getElementById('history-btn').classList.remove('bg-green-300');
         showSectionById('main-donation');
+
     });
 
 document.getElementById('history-btn')
     .addEventListener('click', function () {
-
+        document.getElementById('history-btn').classList.add('bg-green-300');
+        document.getElementById('donation-btn').classList.remove('bg-green-300');
         showSectionById('donation-history');
 
     })
